3b00c64e68f6512fa7ca9fefcfde5fb84d35e314
dlyr <github@dlyr.fr>
2022-05-01 23:08:39 +0200

    fix the fix ... that's why test exists

--- example/main.cpp
+++ example/main.cpp
@@ -2,31 +2,37 @@
 
 #include "src/PowerSlider.hpp"
 
-int main(int argc, char *argv[]) {
-  QApplication app(argc, argv);
-  QWidget window;
-  window.resize(320, 240);
-  window.setWindowTitle(QApplication::translate("powerslider", "Power Slider"));
+int main( int argc, char* argv[] ) {
+    QApplication app( argc, argv );
+    QWidget window;
+    window.resize( 320, 240 );
+    window.setWindowTitle( QApplication::translate( "powerslider", "Power Slider" ) );
 
-  QPushButton *button =
-      new QPushButton(QApplication::translate("powerslider", "Quit"), &window);
+    QPushButton* button =
+        new QPushButton( QApplication::translate( "powerslider", "Quit" ), &window );
 
-  QVBoxLayout *layout = new QVBoxLayout();
+    QVBoxLayout* layout    = new QVBoxLayout();
+    double alignementValue = 100000;
+    for ( auto i = 0; i < 5; i++ ) {
+        PowerSlider* slider = new PowerSlider( &window, alignementValue );
+        layout->addWidget( slider );
+        slider->setRange( i, pow( 10., double( i ) ) );
+    }
+    for ( auto i = 0; i < 5; i++ ) {
+        PowerSlider* slider = new PowerSlider( &window, alignementValue );
+        layout->addWidget( slider );
+        slider->setRange( 0., pow( 10., -double( i ) ) );
+    }
 
-  for (auto i = 0; i < 5; i++) {
-    PowerSlider *slider = new PowerSlider(&window);
-    layout->addWidget(slider);
-  }
+    layout->addWidget( button );
 
-  layout->addWidget(button);
+    window.setLayout( layout );
 
-  window.setLayout(layout);
+    window.show();
 
-  window.show();
+    button->show();
 
-  button->show();
+    QObject::connect( button, &QPushButton::clicked, &app, &QApplication::quit );
 
-  QObject::connect(button, &QPushButton::clicked, &app, &QApplication::quit);
-
-  return app.exec();
+    return app.exec();
 }
